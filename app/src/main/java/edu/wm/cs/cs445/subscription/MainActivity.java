9cda2a08b08ee87868eecf9f1890b8c2fe2e1672
gaculpepper <gaculpepper@email.wm.edu>
2021-09-29 05:36:29 -0400

    Initial commit. contains nothing but basic hello world project generated from android studio

---
+++ app/src/main/java/edu/wm/cs/cs445/subscription/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.wm.cs.cs445.subscription;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("working");
+    }
+}
